dad30d89c3037daf284ab09a961eb5a5bf7cb3e8
Mohamad Reza Tnh <m.reza.tnh@gmail.com>
2021-11-19 23:41:41 +0330

    rolls and footer compeleted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-
-import React  from "react";
-import {BrowserRouter as Router, Route , Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CardWrapper from "./components/CardsWrapper/CardWrapper";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Slider/Slider";
-import VideoWrapper from "./components/VideoWrapper/VideoWrapper"
-import GroupSlider from "./components/GroupSlider/GroupSlider"
-import PackageSlider from "./components/PackageSlider/PackageSlider"
-import "../src/App.css"
+import VideoWrapper from "./components/VideoWrapper/VideoWrapper";
+import GroupSlider from "./components/GroupSlider/GroupSlider";
+import PackageSlider from "./components/PackageSlider/PackageSlider";
+import "../src/App.css";
 import Gallery from "./components/Gallery/Gallery";
 import Footer from "./components/Footer/Footer";
-import HomePage from "../src/components/HomePage"
-import VerticalSlider from "./components/VerticalSlider/VerticalSlider"
+import HomePage from "../src/components/HomePage";
+import VerticalSlider from "./components/VerticalSlider/VerticalSlider";
 import Rolls from "./components/rolls/Rolls";
 import RegisterMail from "./components/RegisterMail/RegisterMail";
 import ContactUs from "./components/ContactUs/ContactUs";
@@ -19,28 +18,23 @@ import AboutUs from "./components/AboutUs/AboutUs";
 
 import Seminars from "./components/Seminars/Seminars";
 import Questions from "./components/Questions/Questions";
-function App (){
-  return(
-<>
+function App() {
+  return (
+    <>
       <Router>
-      <Navbar/>
-      <Switch>
-        <Route></Route>
-      </Switch>
+        <Navbar />
+        <Switch>
+          <Route></Route>
+        </Switch>
       </Router>
       {/* <HomePage/> */}
-      <Questions/>
-
-   <RegisterMail/>
+      {/* <Questions/> */}
+      {/* <Rolls /> */}
+      <AboutUs />
+      <RegisterMail />
       {/* <Gallery/> */}
-      <Footer/>
-
-
-
-
-</>
-  )
- 
-
+      <Footer />
+    </>
+  );
 }
 export default App;
--- src/components/RegisterMail/RegisterMail.js
+++ src/components/RegisterMail/RegisterMail.js
@@ -1,25 +1,32 @@
 import React from "react";
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import "./RegisterMail.css"
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import "./RegisterMail.css";
 function RegisterMail() {
-    return (
-        <div className="register-holder">
-            <div className="register">
-        
-            <Button variant="contained" className="btn-register">ثبت</Button>
-           
-            
-            <TextField id="filled-basic" label="ایمیل" variant="filled" className="input-register" />
-           
-            <div className="info-register">
-                <h4>ایمیل خبرنامه<i className="fa fa-mail"></i></h4>
-                <p>ارسال اخبار و مقالات</p>
-            </div>
-        
-    </div>
-          </div>
-    )
+  return (
+    <div className="register-holder">
+      <div className="register">
+        <div className="register-box">
+          <Button variant="contained" className="btn-register">
+            ثبت
+          </Button>
 
+          <TextField
+            id="filled-basic"
+            label="ایمیل"
+            variant="filled"
+            className="input-register"
+          />
+        </div>
+
+        <div className="info-register">
+          <h4>
+            ایمیل خبرنامه<i className="fa fa-mail"></i>
+          </h4>
+          <p>ارسال اخبار و مقالات</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default RegisterMail;
