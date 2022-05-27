45b60d41f809786d45d1df6f50bb4ecb26a54237
Mohamad Reza Tnh <m.reza.tnh@gmail.com>
2022-05-27 21:16:35 +0430

    distroy white space from desktop version

--- src/components/CardsWrapper/Cards.js
+++ src/components/CardsWrapper/Cards.js
@@ -1,56 +1,51 @@
-import React from "react";
-import Card from "./Card";
+import React from "react"
+import Card from "./Card"
 import "./Card.css"
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import Box from "@mui/material/Box"
+import Paper from "@mui/material/Paper"
+import Grid from "@mui/material/Grid"
 import imgazmoon from "../../Assets/images/آزمون.ico"
 import imgqarbal from "../../Assets/images/غربالگری (1).ico"
 import imgestedad from "../../Assets/images/استعدادیابی.ico"
 import imgmoshavere from "../../Assets/images/مشاوره.ico"
-function Cards(){
-    const cardsInfo=[
-     {
-         key:1,
-         images:imgazmoon,
-         text:"آزمون"
-     },
-     {
-        key:2,
-        images:imgqarbal,
-        text:"غربال گری"
+import { Container } from "@mui/material"
+function Cards() {
+  const cardsInfo = [
+    {
+      key: 1,
+      images: imgazmoon,
+      text: "آزمون"
     },
     {
-        key:3,
-        images:imgestedad,
-        text:"استعداد یابی"
+      key: 2,
+      images: imgqarbal,
+      text: "غربال گری"
     },
     {
-        key:4,
-        images:imgmoshavere,
-        text:"مشاوره"
+      key: 3,
+      images: imgestedad,
+      text: "استعداد یابی"
+    },
+    {
+      key: 4,
+      images: imgmoshavere,
+      text: "مشاوره"
     }
- 
-    ]
-    return(
-        <Box className="cards-wrapper "  justify="center" sx={{ flexGrow: 1 }}>
-            
-            <Grid className="cards" container   justify="flex-end"
-alignItems="center" spacing={3}>
-                {cardsInfo.map((items,index)=>{
-                    return(
-                        <Grid item xs={12} sm={6} md={3} key={index}>
-                           <Card key={index} item={items}/>
-                      </Grid>
-
-                  
-                    )
-                })}
-            </Grid>
-        </Box>
-    )
-
+  ]
+  return (
+    <Container maxWidth="lg">
+      <Box className="cards-wrapper ">
+        <Grid className="cards" container spacing={4}>
+          {cardsInfo.map((items, index) => {
+            return (
+              <Grid item xs={12} sm={6} md={3} key={index}>
+                <Card key={index} item={items} />
+              </Grid>
+            )
+          })}
+        </Grid>
+      </Box>
+    </Container>
+  )
 }
-export default Cards;
-
-
+export default Cards
--- src/components/VideoWrapper/VideoWrapper.js
+++ src/components/VideoWrapper/VideoWrapper.js
@@ -1,22 +1,39 @@
-import { Grid } from "@mui/material";
-import React from "react";
+import { Grid } from "@mui/material"
+import React from "react"
 import "./VideoWrapper.css"
-function VideoWrapper (){
-    return(
-        <Grid container spacing={2} className="section3">
-        <Grid className="video-wrapper" item xs={12} sm={12} md={6}>
-            <h3 className="header-video">یک دقیقه با روشا</h3>
-            <p className="text-video">مناسب برای خود فکر... جای آنکه به انتخاب های رایج در جامعه فکــــــــــــــــــر کند، به انتخاب درســـت در زندگی اش کمک می کند. در واقع می شـــود گفت او »به شــناســایی درســـت اســـتعدادها، به افراد در داشـــتن انتخاب های گیری آینده تحصـــــــــــــيلي و شغلي افراد خواهد بود. به همین دلیل مطالعه و بهبود اين ويژگيهاي ذاتي راهگشــــــــــاي موفقيت و جهت اســـــتعدادها و گونههاي رفتاري متفاوتي اســـــت كه شــــــناســــــايي، انسان يكي از پيچيده ترين مخلوقات عالم هستي، داراي قابليتها،<strong className=""><a href="">ادامه مطلب</a></strong></p>
-            
-        </Grid>
-        <Grid className="video-holder" item xs={12} sm={12} md={6} >
-           <div className="border-video">
-           <video controls poster="https://upload.wikimedia.org/wikipedia/commons/d/d5/Pl3.jpg">
-            <source src="../../../src/Assets/videos/video11.mp4" type="video/mp4"/> 
-            </video>
-           </div>
-        </Grid>
-        </Grid>
-    )
+function VideoWrapper() {
+  return (
+    <Grid container className="section3">
+      <Grid className="video-wrapper" item xs={12} sm={12} md={6}>
+        <h3 className="header-video">یک دقیقه با روشا</h3>
+        <p className="text-video">
+          مناسب برای خود فکر... جای آنکه به انتخاب های رایج در جامعه
+          فکــــــــــــــــــر کند، به انتخاب درســـت در زندگی اش کمک می کند.
+          در واقع می شـــود گفت او »به شــناســایی درســـت اســـتعدادها، به
+          افراد در داشـــتن انتخاب های گیری آینده تحصـــــــــــــيلي و شغلي
+          افراد خواهد بود. به همین دلیل مطالعه و بهبود اين ويژگيهاي ذاتي
+          راهگشــــــــــاي موفقيت و جهت اســـــتعدادها و گونههاي رفتاري متفاوتي
+          اســـــت كه شــــــناســــــايي، انسان يكي از پيچيده ترين مخلوقات عالم
+          هستي، داراي قابليتها،
+          <strong className="">
+            <a href="">ادامه مطلب</a>
+          </strong>
+        </p>
+      </Grid>
+      <Grid className="video-holder" item xs={12} sm={12} md={6}>
+        <div className="border-video">
+          <video
+            controls
+            poster="https://upload.wikimedia.org/wikipedia/commons/d/d5/Pl3.jpg"
+          >
+            <source
+              src="../../../src/Assets/videos/video11.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      </Grid>
+    </Grid>
+  )
 }
-export default VideoWrapper;
+export default VideoWrapper
